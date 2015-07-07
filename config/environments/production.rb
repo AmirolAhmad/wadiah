a426583d880a6cd0143caaea15382dd3afcef12e
amirolahmad <9w2pdf@gmail.com>
2015-07-08 02:39:17 +0800

    compile assets and setup the env mandrilapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf)
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -76,4 +79,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'wadiahsaving.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.mandrillapp.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV["MANDRILL_USERNAME"],
+    :password       => ENV["MANDRILL_PASSWORD"],
+    :domain         => 'wadiahsaving.com',
+    :enable_starttls_auto => true
+  }
 end
